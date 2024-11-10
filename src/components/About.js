6c48bc25326442185983d72aa83f45df16cfb284
Sanat <sanatkakadiya55@gmail.com>
2024-11-10 20:42:48 +0100

    bug fixes

--- src/components/About.js
+++ src/components/About.js
@@ -1,42 +1,49 @@
 import React from "react";
 import Team from "./Team";
-import '../assets/styles/style.css';
-import heroImage from '../assets/images/hero_image.png'
+import "../assets/styles/style.css";
+import heroImage from "../assets/images/hero_image.png";
+import SwiperComponent from "./Swiper";
 
 const About = () => (
   <>
-
-<div className=" bg-black rounded-lg mx-auto min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8">
-    <div className="container mx-auto flex flex-col md:flex-row items-center p-6 sm:p-12">
-      
-      {/* Hero Image Section */}
-      <div className="w-full md:w-1/2 mb-8 md:mb-0 animate__animated animate__backInLeft">
-        <img
-          src={heroImage}
-          alt="Tech solutions hero image"
-          className="w-full h-auto rounded-lg"
-        />
+    <div className=" bg-black rounded-lg mx-auto min-h-screen flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
+      <div className="font-semibold text-silver_main text-xl text-center sm:text-2xl md:text-3xl lg:text-4xl block pt-12 ">
+        INNOVATE, AUTOMATE, ELEVATE
       </div>
+      <div className="container mx-auto flex flex-col md:flex-row items-center p-6 sm:px-12">
+        {/* Hero Image Section */}
+        <div className="w-full md:w-1/2 mb-8 md:mb-0 animate__animated animate__backInLeft">
+          <img
+            src={heroImage}
+            alt="Tech solutions hero image"
+            className="w-full h-auto rounded-lg"
+          />
+        </div>
 
-      {/* Text Section */}
-      <div className="w-full md:w-1/2 text-justify md:text-left animate__animated animate__backInRight">
-        <p className="text-silver_main text-justify leading-relaxed text-base sm:text-lg md:text-xl lg:text-2xl">
-          <span className="font-semibold text-xl sm:text-2xl md:text-3xl lg:text-4xl block mb-4">
-            Welcome to TECHBLEND
-          </span>
-          – a platform where emerging student talent in design, development, and digital strategy converges. Our team of passionate is dedicated to learning and exploring advanced technologies such as Blockchain Technology, AI and Full stake development while creating impactful digital solutions.
-          <br />
-          <br />
-          As students, our goal is to bring fresh perspectives to the field and develop future-ready skills by working on real-world projects. Join us at TechBlend as we build innovative and meaningful digital experiences together, one project at a time.
-        </p>
+        {/* Text Section */}
+        <div className="w-full md:w-1/2 text-justify md:text-left animate__animated animate__backInRight">
+          <p className="text-silver_main text-justify leading-relaxed text-base sm:text-lg md:text-xl lg:text-2xl">
+            <span className="font-semibold text-xl sm:text-2xl md:text-3xl lg:text-4xl block mb-4">
+              Welcome to TECHBLEND
+            </span>
+            – a platform where emerging student talent in design, development,
+            and digital strategy converges. Our team of passionate is dedicated
+            to learning and exploring advanced technologies such as Blockchain
+            Technology, AI and Full stake development while creating impactful
+            digital solutions.
+            <br />
+            <br />
+            As students, our goal is to bring fresh perspectives to the field
+            and develop future-ready skills by working on real-world projects.
+            Join us at TechBlend as we build innovative and meaningful digital
+            experiences together, one project at a time.
+          </p>
+        </div>
       </div>
-      
-
     </div>
-  </div>
-
-  <Team />
-</>
+    {/* <SwiperComponent /> */}
+    <Team />
+  </>
 );
 
 export default About;
--- src/components/Team.js
+++ src/components/Team.js
@@ -6,6 +6,8 @@ import anushkaImage from "../assets/images/anushka.png";
 import aartiImage from "../assets/images/aarti.png";
 import shehlaImage from "../assets/images/shehla.png";
 import Product from "./Product";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Tooltip } from "react-tooltip";
 
 const teamMembers = [
   {
@@ -14,6 +16,8 @@ const teamMembers = [
     description:
       "Passionate about web development, particularly in D-Apps(Decentrelized Applications development using bloackchain)",
     image: sanatImage,
+    github: "https://github.com/Sanat0505/",
+    linkedin: "https://www.linkedin.com/in/sanat-kakadiya-90a2b6190/",
   },
   {
     name: "Anushka Jajal",
@@ -21,6 +25,8 @@ const teamMembers = [
     description:
       "I am a backend developer specializing in Python and Django, with a strong foundation in building scalable web applications. ",
     image: anushkaImage,
+    github: "https://github.com/Annnu04",
+    linkedin: "https://www.linkedin.com/in/anushka-jajal-687760307/",
   },
   {
     name: "Asmaa Abubader",
@@ -28,6 +34,8 @@ const teamMembers = [
     description:
       "A UX Designer in progress ( as entry level). Having a solid knowledge using Mysql",
     image: asmaImage,
+    github: "https://github.com/as650",
+    linkedin: "#",
   },
   {
     name: "Aarti Majumdar",
@@ -35,6 +43,8 @@ const teamMembers = [
     description:
       "Deeply committed to advancing web and API development, utilizing  frontend development expertise to build responsive, intuitive, and high-performing digital solutions.",
     image: aartiImage,
+    github: "#",
+    linkedin: "https://www.linkedin.com/in/aarti-majumdar-4b2395172/",
   },
   {
     name: "Shehla Shehla",
@@ -42,6 +52,8 @@ const teamMembers = [
     description:
       "A dedicated and enthusiastic individual with a passion for Software Development and Backend Development.",
     image: shehlaImage,
+    github: "https://github.com/ENGRSHEHLA",
+    linkedin: "https://www.linkedin.com/in/shehla-shehla-634865211/",
   },
 ];
 
@@ -66,20 +78,40 @@ const Team = () => {
       {/* Team Section */}
 
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-5 md:p-10 max-w-6xl mx-auto">
+        <Tooltip
+        className="max-lg:hidden"
+          id="my-tooltip"
+          style={{ backgroundColor: "rgb(0, 0, 0)", color: "#C0C0C0" }}
+        />
         {teamMembers.map((member, index) => (
           <div
             key={index}
             className="bg-white hover:cursor-pointer rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300"
+            data-tooltip-id="my-tooltip"
+            data-tooltip-content={member.description}
+            data-tooltip-place={`${index + 1 < 4 ? "top" : "bottom"}`}
           >
             <img
               src={member.image}
               alt={member.name}
               className="w-full h-[calc(50vh-64px)] object-cover object-center"
+              
             />
             <div className="p-6 text-justify">
               <h3 className="text-2xl font-semibold">{member.name}</h3>
-              <p className="text-sm text-gray-500">{member.role}</p>
-              <p className="mt-3 text-gray-700">{member.description}</p>
+              <div className="flex justify-between items-center">
+                <p className="text-sm text-gray-500">{member.role}</p>
+                <div className="text-lg flex justify-center items-center gap-2 text-gray-500">
+                  <a target="_blank" href={member.github}>
+                    <FaGithub className="hover:text-black" />
+                  </a>
+                  <a target="_blank" href={member.linkedin}>
+                    <FaLinkedin className="hover:text-blue_main" />
+                  </a>
+                </div>
+              </div>
+
+              {/* <p className="mt-3 text-gray-700">{member.description}</p> */}
             </div>
           </div>
         ))}
